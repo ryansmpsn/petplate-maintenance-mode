8acfa9c6f7e979ec35c95aaf023f3a08a1192357
Ryan Simpson <ryzizn@gmail.com>
2023-09-08 16:13:48 -0400

    feat: petplate maintenance app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import logo from './images/logo.png';
 import './App.css';
+import DesktopDogIllustration from './images/DesktopDogIllustration.png';
+import MobileDogIllustration from './images/MobileDogIllustration.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <img src={logo} className="petplate-logo" alt="petplate logo" />
       </header>
+
+      <main className="main">
+        <div className="content">
+          <h1 className="title">We’ll be right back</h1>
+          <p className="text">
+            We are busy updating PetPlate for you. Please check back soon.
+          </p>
+        </div>
+
+        <div className="image-container">
+          <img
+            src={MobileDogIllustration}
+            className="mobile-illustration"
+            alt="Illustration of a dog jumping"
+          />
+          <img
+            src={DesktopDogIllustration}
+            className="desktop-illustration"
+            alt="Illustration of a dog jumping"
+          />
+        </div>
+      </main>
     </div>
   );
 }
